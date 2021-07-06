fc7928f00fb947588fa55d0bda07f3713d1cb4c7
M-Pheni <50626016+M-Pheni@users.noreply.github.com>
2021-07-06 19:29:29 +0200

    Customize the blog page to list post

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,16 @@ Encore
     // Post css and autoprefixer
     .enablePostCssLoader()
 
+    .copyFiles({
+        from: './assets/images',
+        // optional target path, relative to the output dir
+        //to: 'images/[path][name].[ext]',
+        // if versioning is enabled, add the file hash too
+        
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets',
+    })
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
